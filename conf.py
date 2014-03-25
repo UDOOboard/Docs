19f6001444749de7a823f1761b93965893ead01a
UDOOboard <social@udoo.org>
2014-03-25 15:25:45 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -93,7 +93,7 @@ pygments_style = 'sphinx'
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+#if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
