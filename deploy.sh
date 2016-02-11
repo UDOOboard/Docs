e58da0b02e780a5fe33811a1ff924324d83bb0cd
Francesco Montefoschi <francesco.monte@gmail.com>
2016-02-11 10:43:19 +0100

    New daux

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# clear and re-create the out directory
-rm -rf out || exit 0;
-mkdir out;
-
 # Compile 
-./compile.sh
-#php generate.php global.json out
+./daux.phar
 
 # go to the out directory and create a *new* Git repo
-cd out
+cd static
 git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "social@udoo.org"
+
+# Add drivers and tools
+cp -rp ../driversandtools ../img .
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
@@ -26,6 +24,7 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+echo "Deploying"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-
+#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages 
 
---
+++ templates/partials/navbar_content.php
@@ -0,0 +1,4 @@
+<a class="udoologo pull-left" href="<?= $params['base_page'] . $params['index']->getUri(); ?>">
+		<img src="http://www.udoo.org/docs/img/logo_docs.png"> <span>Docs</span>
+		<a class="btn btn-lg button_docs pull-right" href="http://www.udoo.org/docs-neo/">UDOO Neo Docs</a>
+</a>
